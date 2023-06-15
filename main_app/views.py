379cbc17ffdc6fd22217a43055b8a4e244347aa6
j-vaz12 <jasonvasquezx@gmail.com>
2023-06-14 19:56:19 -0700

    done

--- main_app/views.py
+++ main_app/views.py
@@ -70,7 +70,11 @@ class ToyDelete(DeleteView):
   model = Toy
   success_url = '/toys'
 
-def assoc_toy(request, cat_id, toy_id):
+def assoc_toy(request, finch_id, toy_id):
   # Note that you can pass a toy's id instead of the whole toy object
-  Cat.objects.get(id=cat_id).toys.add(toy_id)
-  return redirect('detail', cat_id=cat_id)
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
